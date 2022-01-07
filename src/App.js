2c9b2aab7f06cc4d4436af8aa4608ac548820abc
dorimusz <78429829+dorimusz@users.noreply.github.com>
2022-01-07 10:02:48 +0100

    initial commit on new branch

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
 import './App.css';
-// import Button from './Button';
-import ButtonListFor from './ButtonListFor'
-import ButtonListMap from './ButtonListMap'
-import Profile from './Profile'
-import Profile2 from './Profile2'
-import Profile3 from './Profile3'
-import Profile4 from './Profile4'
 
+const obj1 = {
+  key1: "random stuff",
+  key2: obj3
+}
 
-const buttonText = "I'm a button component which uses a string from another component";
+const obj2 = {
+  key1: "random stuff 2",
+  key2: obj3
+}
 
+const obj3 = {
+  key1: "random stuff 3"
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
 
-        <div className='hahaha'></div>
-        {/* <Button text={buttonText} /> */}
-        {/* <ButtonListFor text={buttonText} /> */}
-        <ButtonListMap text={buttonText} />
-
-        <Profile />
-        <Profile2 />
-        <Profile3 />
-        <Profile4 />
-
-      </header>
     </div>
   );
 }
--- src/ButtonListMap.js
+++
@@ -1,9 +0,0 @@
-import Button from './Button'
-
-const buttonListMap = () => {
-    const buttonTexts = ["save", "click", "send", "delete", "cancel"];
-
-    return <div>{buttonTexts.map((text, i) => <Button text={text} key={i} />)}</div>
-}
-
-export default buttonListMap;
