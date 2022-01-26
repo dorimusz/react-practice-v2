6410a919e7cb9e899d0309c33be7e929746141d6
dorimusz <78429829+dorimusz@users.noreply.github.com>
2022-01-26 09:21:01 +0100

    titles

--- src/components/ButtonWithHooks.js
+++ src/components/ButtonWithHooks.js
@@ -6,8 +6,9 @@ function ButtonWithHook() {
     // ehhez kell a useState() metódus, aminek adunk egy alapot
 
     const [greeting, setGreeting] = useState("Helloooooo");
+
     /*
-    // A 7-es sor lerövidíti a kövi 3 sort:
+    // A 8-as sor lerövidíti a kövi 3 sort:
     const greetingUseState = useState("Hello2");
     const greeting = greetingUseState[0];
     const setGreeting = greetingUseState[1];
@@ -15,12 +16,22 @@ function ButtonWithHook() {
 
     const [title, setTitle] = useState("I am a new title");
 
+    const [someText, setSomeText] = useState("I'm the base");
+
     return (
         <div>
-            <h1>{title}</h1>
+            <h1>Hooks:</h1>
+            <h2>{title}</h2>
             <p>{greeting}</p>
             <button onClick={() => setGreeting("Licked you")}>Lick me</button>
             <button onClick={() => setTitle("A set a new title for u")}>Click me</button>
+
+
+            <h2>{someText}</h2>
+            <button onClick={() => setSomeText("I am the changed text")}>Click me to change the base text</button>
+
+
+
         </div>
     )
 }
